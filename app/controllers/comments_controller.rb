61faa29077507e63aad04ecc2ac7c3e6c7040dd3
dwlknsn <dwlknsn@users.noreply.github.com>
2024-12-01 12:32:14 +0700

    Update dom for comment deletions via turbo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,11 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        flash[:notice] = "Comment was successfully created."
+        flash.now[:notice] = "Comment was successfully created."
         format.turbo_stream
       else
-        flash[:error] = "Failed with:\n\n#{@comment.errors.full_messages.join("\n")}"
-        redirect_to request.referer
+        flash[:error] = error_message(@comment)
+        format.html { redirect_to request.referer }
       end
     end
   end
@@ -17,9 +17,9 @@ class CommentsController < ApplicationController
     @comment = current_user.authored_comments.find(params.expect(:id))
 
     if @comment.update(comment_params)
-      flash[:notice] = "Comment was successfully updated."
+      flash.now[:notice] = "Comment was successfully updated."
     else
-      flash[:error] = "Failed with:\n\n#{@comment.errors.full_messages.join("\n")}"
+      flash[:error] = error_message(@comment)
     end
 
     redirect_to request.referer
@@ -30,21 +30,24 @@ class CommentsController < ApplicationController
 
     deletion_method = @comment.comments.any? ? :soft_delete! : :destroy!
 
-    if @comment.send(deletion_method)
-      flash[:notice] = "Comment was successfully destroyed."
-    else
-      flash[:error] = "Failed with:\n\n#{@comment.errors.full_messages.join("\n")}"
+    respond_to do |format|
+      if @comment.send(deletion_method)
+        flash.now[:notice] = "Comment was successfully destroyed."
+        format.turbo_stream
+      else
+        flash[:error] = error_message(@comment)
+        format.html { redirect_to request.referer }
+      end
     end
-
-    redirect_to request.referer
   end
 
   private
 
-  def set_post
-  end
-
   def comment_params
     params.expect(comment: [ :commentable_id, :commentable_type, :body ])
+  end
+
+  def error_message(comment)
+    "Failed with:\n\n#{@comment.errors.full_messages.to_sentence}"
   end
 end
