502696a0b6893c58b29fc64f2b6b92432653c296
dwlknsn <dwlknsn@users.noreply.github.com>
2024-12-03 12:55:57 +0700

    WIP broadcasting notifications about likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,14 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
+  # can we add counter cache on a polymorphic column, e.g.
+  # likeable_count, scope -> { :likeable_type, :likeable_id }
 
   validates :user_id, presence: true
   validates :user_id, uniqueness: { scope: [ :likeable_id, :likeable_type ] }
   validate :user_cannot_like_own_likeable
+
+  after_create_commit ->() { broadcast_turbo_stream "#{ dom_id(likeable.author) }-notifications-feed", partial: "notifications/like", locals: { likeable: likeable } }
 
   private
 
