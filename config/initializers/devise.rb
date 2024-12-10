80be9b69329a018bb893160e75fcd57bab78e2da
dwlknsn <dwlknsn@users.noreply.github.com>
2024-12-10 16:13:26 +0700

    Use ENV for credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, Rails.application.credentials.github.client_id, Rails.application.credentials.github.client_secret, scope: "read:user,user:email"
+  # config.omniauth :github, Rails.application.credentials.github.client_id, Rails.application.credentials.github.client_secret, scope: "read:user,user:email"
+  config.omniauth :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "read:user,user:email"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
