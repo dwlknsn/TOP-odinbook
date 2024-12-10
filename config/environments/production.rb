bde15fa436399842d9760d47350ea700e9a15dc4
dwlknsn <dwlknsn@users.noreply.github.com>
2024-12-10 17:36:33 +0700

    update puma config to support ipv6 hosts binding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
+  # "0.0.0.0"
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
